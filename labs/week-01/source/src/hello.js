d658c62148b76add7bb8e4f7d05f8a59762bdda5
film1023 <filmgg.com@gmail.com>
2026-07-23 14:19:38 +0700

    feat: migrate and verify LAB 01

---
+++ labs/week-01/source/src/hello.js
@@ -0,0 +1,12 @@
+const student = {
+  name: "teeranai-chaikantha",
+  studentId: "68543210066-5",
+  os: process.platform,
+  node: process.version,
+};
+
+function createGreeting({ name, studentId, os, node }) {
+  return `Hello ${name} (${studentId}) | OS: ${os} | Node: ${node}`;
+}
+
+console.log(createGreeting(student));
